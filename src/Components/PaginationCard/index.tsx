33b18bf6cb9b0ea681ee3a715d47cbf7a65fd0af
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2020-07-20 20:31:50 -0300

    Alteração do component Details para Pokemon

--- src/Components/PaginationCard/index.tsx
+++ src/Components/PaginationCard/index.tsx
@@ -4,30 +4,32 @@ import Card from "../Card";
 import styles from "./styles";
 import {DATA} from "../../data";
 
-export default function PaginationCard() {
-    // let pokes = await pokemons('pokemon');
-    return (
-        <>
-            <View style={styles.searchForm}>
-                <TextInput
-                    style={styles.searchInput}
-                    placeholder="Buscar pokemons por Nome..."
-                    placeholderTextColor="#999"
-                    autoCapitalize="words"
-                    autoCorrect={false}
-                />
-                <TouchableOpacity style={styles.loadButton}>
-                    <Image source={require('../../images/pokebola_button.png')} style={styles.poekbola}/>
-                </TouchableOpacity>
-            </View>
-            <View style={styles.container}>
-                <FlatList
-                    style={styles.lista}
-                    data={DATA}
-                    renderItem={({item}) => (<Card uri={item.uri} pokemon={item.name}/>)}
-                    keyExtractor={item => item.name}
-                    numColumns={2}
-                    horizontal={false}/>
-            </View>
-        </>);
+export default class PaginationCard extends React.Component {
+    render() {
+        // let pokes = await pokemons('pokemon');
+        return (
+            <>
+                <View style={styles.searchForm}>
+                    <TextInput
+                        style={styles.searchInput}
+                        placeholder="Buscar pokemons por Nome..."
+                        placeholderTextColor="#999"
+                        autoCapitalize="words"
+                        autoCorrect={false}
+                    />
+                    <TouchableOpacity style={styles.loadButton}>
+                        <Image source={require('../../images/pokebola_button.png')} style={styles.poekbola}/>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.container}>
+                    <FlatList
+                        style={styles.lista}
+                        data={DATA}
+                        renderItem={({item}) => (<Card uri={item.uri} pokemon={item.name}/>)}
+                        keyExtractor={item => item.name}
+                        numColumns={2}
+                        horizontal={false}/>
+                </View>
+            </>);
+    }
 }
---
+++ src/Components/Pokemon/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import {View} from "react-native";
+import styles from "./styles";
+
+export default class PaginationCard extends React.Component {
+    render() {
+        return (<View style={styles.container}/>);
+    }
+}
