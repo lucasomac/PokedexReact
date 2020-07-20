b2c00c693797ba3a4f07d4c95fe6f779f48d2f70
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2020-07-20 20:15:07 -0300

    Alteração da cor de fundo

--- src/Components/Card/index.tsx
+++ src/Components/Card/index.tsx
@@ -1,33 +1,15 @@
 import React from 'react'
-import {Image, StyleSheet, View} from 'react-native';
-// import styles from './styles';
+import {Image, Text, View} from 'react-native';
+import styles from "./styles";
 
-export default class Card extends React.Component<{ uri: any }> {
+export default class Card extends React.Component<{ uri: any, pokemon: string }> {
     render() {
-        let {uri} = this.props;
+        let {uri, pokemon} = this.props;
         return (
             <View style={styles.container}>
                 <Image source={{uri: uri}} style={styles.tinyLogo}/>
+                <Text style={styles.pokemon}>{pokemon}</Text>
             </View>
         );
     }
 }
-const styles = StyleSheet.create({
-    container: {
-        marginTop: 10,
-        marginBottom: 10,
-        marginRight: 10,
-        width: 200,
-        height: 200,
-        backgroundColor: '#6800ff',
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderStyle: 'solid',
-        borderRadius: 30
-    },
-    tinyLogo: {
-        // flex: 1,
-        width: 100,
-        height: 100,
-    },
-});
