9a9d134042cad9616f8021381e3e9b2e6ac861a4
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2020-07-22 20:22:04 -0300

    Alteração das referncias dos parametros do name

--- src/Components/PaginationCard/index.tsx
+++ src/Components/PaginationCard/index.tsx
@@ -1,45 +1,41 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {FlatList, Image, TextInput, TouchableOpacity, View} from "react-native";
 import Card from "../Card";
 import styles from "./styles";
-import {DATA} from "../../data";
+import {getPokemons} from "../../data";
 
-export default class PaginationCard extends React.Component {
-    // pokes = await pokemons();
-
-    render() {
-        return (
-            <>
-                <View style={styles.searchForm}>
-                    <TextInput
-                        style={styles.searchInput}
-                        placeholder="Buscar pokemons por Nome..."
-                        placeholderTextColor="#999"
-                        autoCapitalize="words"
-                        autoCorrect={false}
-                    />
-                    <TouchableOpacity style={styles.loadButton}>
-                        <Image source={require('../../images/pokebola_button.png')} style={styles.poekbola}/>
-                    </TouchableOpacity>
-                </View>
-                {/*<View style={styles.container}>*/}
-                {/*    <FlatList*/}
-                {/*        style={styles.lista}*/}
-                {/*        data={[this.pokes]}*/}
-                {/*        renderItem={({item}) => (<Card uri={item.url} pokemon={item.name}/>)}*/}
-                {/*        keyExtractor={item => item.name}*/}
-                {/*        numColumns={2}*/}
-                {/*        horizontal={false}/>*/}
-                {/*</View>*/}
-                <View style={styles.container}>
-                    <FlatList
-                        style={styles.lista}
-                        data={DATA}
-                        renderItem={({item}) => (<Card uri={item.uri} pokemon={item.name}/>)}
-                        keyExtractor={item => item.name}
-                        numColumns={2}
-                        horizontal={false}/>
-                </View>
-            </>);
-    }
+export default function PaginationCard() {
+    var [pokemons, setPokemons] = useState([]);
+    var offset = 100, limit = 20;
+    useEffect(() => {
+        getPokemons({offset, limit}).then(response => {
+            setPokemons(response.data.results);
+        });
+    }, []);
+    return (
+        <>
+            <View style={styles.searchForm}>
+                <TextInput
+                    style={styles.searchInput}
+                    placeholder="Buscar pokemons por Nome..."
+                    placeholderTextColor="#999"
+                    autoCapitalize="words"
+                    autoCorrect={false}
+                />
+                <TouchableOpacity style={styles.loadButton}>
+                    <Image source={require('../../images/pokebola_button.png')} style={styles.poekbola}/>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.container}>
+                <FlatList
+                    style={styles.lista}
+                    data={pokemons}
+                    renderItem={({item: {name, url}}) => (<Card
+                        uri={'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/143.png'}
+                        pokemon={name}/>)}
+                    keyExtractor={({name}) => name}
+                    numColumns={2}
+                    horizontal={false}/>
+            </View>
+        </>);
 }
